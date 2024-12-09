e9a46121c4dc4e27e63c3023fa87ac5811caa75f
Nciyash <x23326841@student.ncirl.ie>
2024-12-09 16:57:12 +0000

    created recaptcha.php and updated logout.php
    Added secure cookies and captcha verification

--- Online-Library-Management-System-PHP/library/logout.php
+++ Online-Library-Management-System-PHP/library/logout.php
@@ -1,6 +1,22 @@
 <?php
 session_start(); 
-session_destroy(); // destroy session
-header("location:index.php"); 
-?>
 
+// Unset all session variables
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Destroy the session
+session_destroy(); 
+
+// Redirect to index.php
+header("location:index.php"); 
+exit();
+?>
