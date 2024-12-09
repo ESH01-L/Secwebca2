c19a291716b293d4a822d93e9295e55f6cc60bf0
Nciyash <x23326841@student.ncirl.ie>
2024-12-09 19:40:54 +0000

    update index.php
    implemented secure cookie, session management, secure headers

--- Online-Library-Management-System-PHP/library/index.php
+++ Online-Library-Management-System-PHP/library/index.php
@@ -1,43 +1,71 @@
 <?php
+// Secure cookie settings
+session_set_cookie_params([
+    'lifetime' => 86400,
+    'path' => '/',
+    'domain' => '',
+    'secure' => true,
+    'httponly' => true,
+    'samesite' => 'Strict'
+]);
+
 session_start();
-error_reporting(0);
+
+// Session timeout settings
+$timeout_duration = 1800; // 30 minutes
+
+// Check if the user is logged in and if the session has timed out
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+    // Last request was more than 30 minutes ago
+    session_unset(); // Unset $_SESSION variable for the run-time
+    session_destroy(); // Destroy session data in storage
+    header("Location: signin.php"); // Redirect to login page
+    exit();
+}
+$_SESSION['LAST_ACTIVITY'] = time(); // Update last activity time stamp
+
+// Set secure headers
+header("Content-Security-Policy: default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';");
+header("X-Content-Type-Options: nosniff");
+header("X-Frame-Options: DENY");
+header("X-XSS-Protection: 1; mode=block");
+header("Referrer-Policy: no-referrer");
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 include('includes/config.php');
-if($_SESSION['login']!=''){
-$_SESSION['login']='';
-}
-if(isset($_POST['login']))
-{
 
-$email=$_POST['emailid'];
-$password=md5($_POST['password']);
-$sql ="SELECT EmailId,Password,StudentId,Status FROM tblstudents WHERE EmailId=:email and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':email', $email, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-
-if($query->rowCount() > 0)
-{
- foreach ($results as $result) {
- $_SESSION['stdid']=$result->StudentId;
-if($result->Status==1)
-{
-$_SESSION['login']=$_POST['emailid'];
-echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
-} else {
-echo "<script>alert('Your Account Has been blocked .Please contact admin');</script>";
-
-}
+if (isset($_SESSION['login']) && $_SESSION['login'] != '') {
+    $_SESSION['login'] = '';
 }
 
-} 
+if (isset($_POST['login'])) {
+    $email = filter_var($_POST['emailid'], FILTER_SANITIZE_EMAIL);
+    $password = $_POST['password'];
 
-else{
-echo "<script>alert('Invalid Details');</script>";
-}
-}
+    $sql = "SELECT EmailId, Password, StudentId, Status FROM tblstudents WHERE EmailId = :email";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':email', $email, PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_OBJ);
 
+    if ($result) {
+        if (password_verify($password, $result->Password)) {
+            $_SESSION['stdid'] = $result->StudentId;
+            if ($result->Status == 1) {
+                $_SESSION['login'] = $email;
+                session_regenerate_id(true); // Regenerate session ID to prevent session fixation
+                echo "<script type='text/javascript'> document.location ='dashboard.php'; </script>";
+            } else {
+                echo "<script>alert('Your Account Has been blocked. Please contact admin');</script>";
+            }
+        } else {
+            echo "<script>alert('Invalid Details');</script>";
+        }
+    } else {
+        echo "<script>alert('Invalid Details');</script>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -55,96 +83,85 @@ echo "<script>alert('Invalid Details');</script>";
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
 </head>
 <body>
     <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-<div class="content-wrapper">
-<div class="container">
-<!--Slider---->
-     <div class="row">
-              <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
-                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel" >
-                    <div class="carousel-inner">
-                        <div class="item active">
-                            <img src="assets/img/1.jpg" alt="" />
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+            <!--Slider---->
+            <div class="row">
+                <div class="col-md-10 col-sm-8 col-xs-12 col-md-offset-1">
+                    <div id="carousel-example" class="carousel slide slide-bdr" data-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <img src="assets/img/1.jpg" alt="" />
+                            </div>
+                            <div class="item">
+                                <img src="assets/img/2.jpg" alt="" />
+                            </div>
+                            <div class="item">
+                                <img src="assets/img/3.jpg" alt="" />
+                            </div>
                         </div>
-                        <div class="item">
-                            <img src="assets/img/2.jpg" alt="" />
+                        <!--INDICATORS-->
+                        <ol class="carousel-indicators">
+                            <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-example" data-slide-to="1"></li>
+                            <li data-target="#carousel-example" data-slide-to="2"></li>
+                        </ol>
+                        <!--PREVIUS-NEXT BUTTONS-->
+                        <a class="left carousel-control" href="#carousel-example" data-slide="prev">
+                            <span class="glyphicon glyphicon-chevron-left"></span>
+                        </a>
+                        <a class="right carousel-control" href="#carousel-example" data-slide="next">
+                            <span class="glyphicon glyphicon-chevron-right"></span>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <hr />
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">USER LOGIN FORM</h4>
+                </div>
+            </div>
+            <a name="ulogin"></a>
+            <!--LOGIN PANEL START-->
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            LOGIN FORM
                         </div>
-                        <div class="item">
-                            <img src="assets/img/3.jpg" alt="" /> 
+                        <div class="panel-body">
+                            <form role="form" method="post">
+                                <div class="form-group">
+                                    <label>Enter Email id</label>
+                                    <input class="form-control" type="text" name="emailid" required autocomplete="off" />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="form-control" type="password" name="password" required autocomplete="off" />
+                                    <p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
+                                </div>
+                                <button type="submit" name="login" class="btn btn-info">LOGIN</button> | <a href="signup.php">Not Register Yet</a>
+                            </form>
                         </div>
                     </div>
-                    <!--INDICATORS-->
-                     <ol class="carousel-indicators">
-                        <li data-target="#carousel-example" data-slide-to="0" class="active"></li>
-                        <li data-target="#carousel-example" data-slide-to="1"></li>
-                        <li data-target="#carousel-example" data-slide-to="2"></li>
-                    </ol>
-                    <!--PREVIUS-NEXT BUTTONS-->
-                     <a class="left carousel-control" href="#carousel-example" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-  </a>
                 </div>
-              </div>
-             </div>
-<hr />
-
-
-
-<div class="row pad-botm">
-<div class="col-md-12">
-<h4 class="header-line">USER LOGIN FORM</h4>
-</div>
-</div>
- <a name="ulogin"></a>            
-<!--LOGIN PANEL START-->           
-<div class="row">
-<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
-<div class="panel panel-info">
-<div class="panel-heading">
- LOGIN FORM
-</div>
-<div class="panel-body">
-<form role="form" method="post">
-
-<div class="form-group">
-<label>Enter Email id</label>
-<input class="form-control" type="text" name="emailid" required autocomplete="off" />
-</div>
-<div class="form-group">
-<label>Password</label>
-<input class="form-control" type="password" name="password" required autocomplete="off"  />
-<p class="help-block"><a href="user-forgot-password.php">Forgot Password</a></p>
-</div>
-
-
-
- <button type="submit" name="login" class="btn btn-info">LOGIN </button> | <a href="signup.php">Not Register Yet</a>
-</form>
- </div>
-</div>
-</div>
-</div>  
-<!---LOGIN PABNEL END-->            
-             
- 
+            </div>
+            <!---LOGIN PANEL END-->
+        </div>
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
- <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php'); ?>
+    <!-- FOOTER SECTION END-->
     <script src="assets/js/jquery-1.10.2.js"></script>
     <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
+    <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
-
 </body>
 </html>
