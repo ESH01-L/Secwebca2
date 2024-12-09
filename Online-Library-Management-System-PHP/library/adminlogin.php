9566933e1ad2b65f76184cfdcf7daa752fdcdfa5
Nciyash <x23326841@student.ncirl.ie>
2024-12-09 18:18:32 +0000

    updated adminlogin.php
    Implemented CSRF token, sanitized input and recaptcha

--- Online-Library-Management-System-PHP/library/adminlogin.php
+++ Online-Library-Management-System-PHP/library/adminlogin.php
@@ -2,26 +2,39 @@
 session_start();
 error_reporting(0);
 include('includes/config.php');
-if($_SESSION['alogin']!=''){
-$_SESSION['alogin']='';
+
+if ($_SESSION['alogin'] != '') {
+    $_SESSION['alogin'] = '';
 }
-if(isset($_POST['login']))
-{
-$username=$_POST['username'];
-$password=md5($_POST['password']);
-$sql ="SELECT UserName,Password FROM admin WHERE UserName=:username and Password=:password";
-$query= $dbh -> prepare($sql);
-$query-> bindParam(':username', $username, PDO::PARAM_STR);
-$query-> bindParam(':password', $password, PDO::PARAM_STR);
-$query-> execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
-if($query->rowCount() > 0)
-{
-$_SESSION['alogin']=$_POST['username'];
-echo "<script type='text/javascript'> document.location ='admin/dashboard.php'; </script>";
-} else{
-echo "<script>alert('Invalid Details');</script>";
+
+if (isset($_POST['login'])) {
+    // CSRF token validation
+    if (!hash_equals($_SESSION['token'], $_POST['token'])) {
+        echo "<script>alert('Invalid CSRF token');</script>";
+        exit;
+    }
+
+    $username = htmlspecialchars($_POST['username']);
+    $password = $_POST['password'];
+
+    $sql = "SELECT UserName, Password FROM admin WHERE UserName = :username";
+    $query = $dbh->prepare($sql);
+    $query->bindParam(':username', $username, PDO::PARAM_STR);
+    $query->execute();
+    $result = $query->fetch(PDO::FETCH_OBJ);
+
+    if ($result && password_verify($password, $result->Password)) {
+        session_regenerate_id(true); // Prevent session fixation
+        $_SESSION['alogin'] = $username;
+        echo "<script type='text/javascript'> document.location ='admin/dashboard.php'; </script>";
+    } else {
+        echo "<script>alert('Invalid Details');</script>";
+    }
 }
+
+// Generate CSRF token
+if (empty($_SESSION['token'])) {
+    $_SESSION['token'] = bin2hex(random_bytes(32));
 }
 ?>
 <!DOCTYPE html>
@@ -40,58 +53,60 @@ echo "<script>alert('Invalid Details');</script>";
     <link href="assets/css/style.css" rel="stylesheet" />
     <!-- GOOGLE FONT -->
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css' />
-
 </head>
 <body>
     <!------MENU SECTION START-->
-<?php include('includes/header.php');?>
-<!-- MENU SECTION END-->
-<div class="content-wrapper">
-<div class="container">
-<div class="row pad-botm">
-<div class="col-md-12">
-<h4 class="header-line">ADMIN LOGIN FORM</h4>
-</div>
-</div>
-             
-<!--LOGIN PANEL START-->           
-<div class="row">
-<div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3" >
-<div class="panel panel-info">
-<div class="panel-heading">
- LOGIN FORM
-</div>
-<div class="panel-body">
-<form role="form" method="post">
-
-<div class="form-group">
-<label>Enter Username</label>
-<input class="form-control" type="text" name="username" autocomplete="off" required />
-</div>
-<div class="form-group">
-<label>Password</label>
-<input class="form-control" type="password" name="password" autocomplete="off" required />
-</div>
-
- <button type="submit" name="login" class="btn btn-info">LOGIN </button>
-</form>
- </div>
-</div>
-</div>
-</div>  
-<!---LOGIN PABNEL END-->            
-             
- 
+    <?php include('includes/header.php'); ?>
+    <!-- MENU SECTION END-->
+    <div class="content-wrapper">
+        <div class="container">
+            <div class="row pad-botm">
+                <div class="col-md-12">
+                    <h4 class="header-line">ADMIN LOGIN FORM</h4>
+                </div>
+            </div>
+            <!--LOGIN PANEL START-->
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                    <div class="panel panel-info">
+                        <div class="panel-heading">
+                            LOGIN FORM
+                        </div>
+                        <div class="panel-body">
+                            <form role="form" method="post">
+                                <input type="hidden" name="token" value="<?php echo $_SESSION['token']; ?>" />
+                                <div class="form-group">
+                                    <label>Enter Username</label>
+                                    <input class="form-control" type="text" name="username" autocomplete="off" required />
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input class="form-control" type="password" name="password" autocomplete="off" required />
+                                </div>
+                                <div class="g-recaptcha" data-sitekey="6Lcnn5EqAAAAAPPAlTqjznykTMTrj44vj5ZVxsXM" data-callback="enablesubmitbtn"></div>
+                                <script src="https://www.google.com/recaptcha/api.js" async defer></script><br>
+                                <input type="submit" id="submit" class="btn btn-info" disabled="disabled" name="login" value="Login"><br><br>
+                                <script>
+                                    function enablesubmitbtn() {
+                                        document.getElementById("submit").disabled = false;
+                                    }
+                                </script>
+                               
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!---LOGIN PANEL END-->
+        </div>
     </div>
-    </div>
-     <!-- CONTENT-WRAPPER SECTION END-->
- <?php include('includes/footer.php');?>
-      <!-- FOOTER SECTION END-->
+    <!-- CONTENT-WRAPPER SECTION END-->
+    <?php include('includes/footer.php'); ?>
+    <!-- FOOTER SECTION END-->
     <script src="assets/js/jquery-1.10.2.js"></script>
     <!-- BOOTSTRAP SCRIPTS  -->
     <script src="assets/js/bootstrap.js"></script>
-      <!-- CUSTOM SCRIPTS  -->
+    <!-- CUSTOM SCRIPTS  -->
     <script src="assets/js/custom.js"></script>
-</script>
 </body>
 </html>
